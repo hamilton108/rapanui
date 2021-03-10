eeba2fe56145a148082641ba69df407953bd53bb
Hamilton 108 <hamilton@hamilton.com>
2021-03-10 13:53:16 +0100

    Deleted files

--- src/clojure/rapanui/protocols/derivatives.clj
+++
@@ -1,6 +0,0 @@
-(ns rapanui.protocols.derivatives)
-
-(defprotocol IPurchase
-  (strutMyStuff [this])
-  (whoami [this])
-  (applyCritters [this]))
--- src/main/java/rapanui/sellrules/DefaultCritters.java
+++
@@ -1,5 +0,0 @@
-package rapanui.sellrules;
-
-public class DefaultCritters {
-
-}
